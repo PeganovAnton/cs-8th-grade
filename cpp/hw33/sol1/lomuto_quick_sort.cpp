852fe427dd02a2890e2f18b51f2cfb9aeed688fc
PeganovAnton <peganoff2@mail.ru>
2020-08-16 17:00:37 +0300

    Add hw33 hw34

---
+++ cpp/hw33/sol1/lomuto_quick_sort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void print_array(int *a, int len)
+{
+   for(int i=0; i<len; ++i)
+     cout << a[i] << ' ';
+   cout << endl;
+}
+
+
+void swap(int *a, int i, int j)
+{
+  int tmp = a[i];
+  a[i] = a[j];
+  a[j] = tmp;
+}
+
+
+int lomuto_partition(int *a, int lo, int hi)
+{
+  int pivot = a[hi];
+  int left = lo;
+  while(a[left] < pivot)
+    ++left;
+  for(int right=left+1; right<hi; ++right){
+    if (a[right] < pivot) {
+      swap(a, left, right);
+      ++left;
+    }
+  }
+  swap(a, left, hi);
+  return left;
+}
+
+
+int lomuto_search(int *a, int lo, int hi, int k)
+{
+        if (hi - lo < 1)
+            return a[k];
+        int p = lomuto_partition(a, lo, hi);
+        if(k < p)
+            lomuto_search(a, lo, p-1, k);
+        if(k > p)
+            lomuto_search(a, p+1, hi, k);
+}
+
+
+int main()
+{
+  int a[10] = {2, 1, 6, 3, 10, 5, 4, 8, 7, 9};
+  int k;
+  cin >> k;
+  cout << lomuto_search(a, 0, 9, k) << endl;
+  print_array(a, 10);
+
+}
+
